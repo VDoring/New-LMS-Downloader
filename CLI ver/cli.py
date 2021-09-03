4c9abf7fb702f8e6193270f14e47f959c1be4ef6
Doring <d0211468@kowon.dongseo.ac.kr>
2021-09-04 01:03:35 +0900

    v1.0.3
    다운로드 방법 2가지를 추가하였습니다.

--- CLI ver/cli.py
+++ CLI ver/cli.py
@@ -1,27 +1,51 @@
-# urllib는 requests 라이브러리를 설치하면 사용할 수 있습니다. #
 import urllib.request
 import os
+from requests import get
 
 os.system('mode con cols=134 lines=17')
-os.system('title New LMS Video Downloader - CLI type v1.0.2 by VDoring')
+os.system('title New LMS Video Downloader - CLI type v1.0.3 by VDoring')
 
 print('< LMS 영상 다운로드 프로그램 >')
 print('사용방법: https://github.com/VDoring/New-LMS-Downloader\n')
 
 i = 1
 
-def videoSave(videolink, filename):
-    try:
+def videoSaveManager(videolink, filename):
+    try: # 시도1
         print('다운로드중...', end='', flush=True)
-        urllib.request.urlretrieve(videolink, filename)
+        videoSave1(videolink, filename)
         print('\r>', filename, '저장이 완료되었습니다! <\n')
     except:
-        print('\r> 올바른 링크가 아닙니다! <\n')
+        try: # 시도2
+            videoSave2(videolink, filename)
+            print('\r>', filename, '저장이 완료되었습니다! <\n')
+        except:
+            try: # 시도3
+                videoSave3(videolink, filename)
+                print('\r>', filename, '저장이 완료되었습니다! <\n')
+            except:
+                print('\r> 다운로드에 실패했습니다. <\n')
+
+
+def videoSave1(videolink, filename):
+    urllib.request.urlretrieve(videolink, filename)
+
+def videoSave2(videolink, filename):
+    with open(filename, "wb") as file:
+        response = get(videolink)
+        file.write(response.content)
+
+def videoSave3(videolink, filename):
+    video_read = urllib.request.urlopen(videolink).read()
+    video_open = open(filename, 'wb')
+    video_open.write(video_read)
+    video_open.close()
 
 while True:
     print('[' + str(i) + '번째 다운로드' + ']')
     user_videolink = input('영상 링크를 입력하세요: ')
     user_filename = input('파일 이름을 입력하세요: ')
     user_filename = user_filename + '.mp4'
-    videoSave(user_videolink, user_filename)
+
+    videoSaveManager(user_videolink, user_filename)
     i += 1
